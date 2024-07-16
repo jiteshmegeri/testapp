81c3fa486c07e9b4e771403882e2cd158c1a5b9f
jiteshmegeri <surjitmegeri@gmail.com>
2024-07-16 17:28:42 +0530

    f

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+  
+
+app.listen();
